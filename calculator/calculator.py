89f73911f2f35a4f13ad90265e5e28c4e5b49ac0
Your Name <you@example.com>
2024-01-31 18:02:52 +0530

    calculator project

---
+++ calculator/calculator.py
@@ -0,0 +1,13 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============================================== RESTART: C:/Users/acer/AppData/Local/Programs/Python/Python311/csl.py ==============================================
+Select operation.
+1.Add
+2.Subtract
+3.Multiply
+4.Divide
+Enter choice(1/2/3/4): 1
+Enter first number: 2
+Enter second number: 3
+2.0 + 3.0 = 5.0
